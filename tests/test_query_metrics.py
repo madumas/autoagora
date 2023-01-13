201bc2e67bb108a5781c3547af121b255f11db5f
Marc-André Dumas <madumas@madumas.ca>
2023-01-13 10:32:36 -0500

    test: change to pytest
    Signed-off-by: madumas <madumas@madumas.ca>

--- tests/test_query_metrics.py
+++ tests/test_query_metrics.py
@@ -1,18 +1,15 @@
-import unittest
 import asyncio
 import autoagora.query_metrics
-
 from autoagora.config import init_config
-init_config(["--indexer-agent-mgmt-endpoint", "http://nowhere",
-             "--indexer-service-metrics-endpoint", "http://indexer-service.default.svc.cluster.local:7300/metrics"])
 
-class MyTestCase(unittest.TestCase):
+init_config(["--indexer-agent-mgmt-endpoint", "http://nowhere",
+         "--indexer-service-metrics-endpoint", "http://indexer-service.default.svc.cluster.local:7300/metrics"])
+
+class MyTestCase:
     def test_subgraph_query_count(self):
         res = asyncio.run(
             autoagora.query_metrics.subgraph_query_count("Qmadj8x9km1YEyKmRnJ6EkC2zpJZFCfTyTZpuqC3j6e1QH")
         )
         print(res)
-        self.assertEqual(res, 928)
+        assert(res == 928)
 
-if __name__ == '__main__':
-    unittest.main()
