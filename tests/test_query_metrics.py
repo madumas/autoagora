c3eace4206f7c305eccf7f28cc132776f155cee5
Marc-André Dumas <madumas@madumas.ca>
2023-01-13 10:39:51 -0500

    test: move vcr logic to test, as it should be
    Signed-off-by: madumas <madumas@madumas.ca>

--- tests/test_query_metrics.py
+++ tests/test_query_metrics.py
@@ -1,15 +1,18 @@
 import asyncio
 import autoagora.query_metrics
+import vcr
 from autoagora.config import init_config
 
 init_config(["--indexer-agent-mgmt-endpoint", "http://nowhere",
          "--indexer-service-metrics-endpoint", "http://indexer-service.default.svc.cluster.local:7300/metrics"])
 
-class MyTestCase:
+
+class TestQueryMetrics:
     def test_subgraph_query_count(self):
-        res = asyncio.run(
-            autoagora.query_metrics.subgraph_query_count("Qmadj8x9km1YEyKmRnJ6EkC2zpJZFCfTyTZpuqC3j6e1QH")
-        )
+        with vcr.use_cassette('vcr_cassettes/indexer_service_metrics_endpoint.yaml'):
+            res = asyncio.run(
+                    autoagora.query_metrics.subgraph_query_count("Qmadj8x9km1YEyKmRnJ6EkC2zpJZFCfTyTZpuqC3j6e1QH")
+            )
         print(res)
         assert(res == 928)
 
