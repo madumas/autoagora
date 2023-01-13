cfb047e108e4f7d41f87bc2f1e8d8a588004ca8e
Marc-André Dumas <madumas@madumas.ca>
2023-01-13 10:43:11 -0500

    test: rename vcr cassette
    Signed-off-by: madumas <madumas@madumas.ca>

--- tests/test_query_metrics.py
+++ tests/test_query_metrics.py
@@ -4,15 +4,14 @@ import vcr
 from autoagora.config import init_config
 
 init_config(["--indexer-agent-mgmt-endpoint", "http://nowhere",
-         "--indexer-service-metrics-endpoint", "http://indexer-service.default.svc.cluster.local:7300/metrics"])
+             "--indexer-service-metrics-endpoint", "http://indexer-service.default.svc.cluster.local:7300/metrics"])
 
 
 class TestQueryMetrics:
     def test_subgraph_query_count(self):
-        with vcr.use_cassette('vcr_cassettes/indexer_service_metrics_endpoint.yaml'):
+        with vcr.use_cassette('vcr_cassettes/test_subgraph_query_count.yaml'):
             res = asyncio.run(
-                    autoagora.query_metrics.subgraph_query_count("Qmadj8x9km1YEyKmRnJ6EkC2zpJZFCfTyTZpuqC3j6e1QH")
+                autoagora.query_metrics.subgraph_query_count("Qmadj8x9km1YEyKmRnJ6EkC2zpJZFCfTyTZpuqC3j6e1QH")
             )
         print(res)
-        assert(res == 928)
-
+        assert (res == 932)
